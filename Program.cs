b8015da879bd2cb590f44e0f507b53c1e4d167f5
志超 葛 <3529820494@qq.com>
2021-03-01 18:15:30 +0800

    expmle

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            
         }
     }
 }
